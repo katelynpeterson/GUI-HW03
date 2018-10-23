05adac633539d2dee52d97c384c472d68ed35347
Katelyn Peterson <00154664@badgermail.snow.edu>
2018-10-23 15:16:21 -0600

    Add new files to project

---
+++ KPeterson_HW03/DateValidationRules.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace KPeterson_HW03
+{
+    class DateValidationRules : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            DateTime start = (DateTime)value;
+            DateTime end = start.AddDays(1);
+            return new ValidationResult(start < end, "End date must be after start date");
+        }
+    }
+}
---
+++ KPeterson_HW03/Dependencies.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KPeterson_HW03
+{
+    class Dependencies : DependencyObject
+    {
+        public DateTime MaxDate
+        {
+            get { return (DateTime)GetValue(MaxDateProperty); }
+            set { SetValue(MaxDateProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MaxDate.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MaxDateProperty =
+            DependencyProperty.Register("MaxDate", typeof(DateTime), typeof(Projects), new PropertyMetadata(0));
+
+
+
+
+        public DateTime MinDate
+        {
+            get { return (DateTime)GetValue(MinDateProperty); }
+            set { SetValue(MinDateProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MinDate.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MinDateProperty =
+            DependencyProperty.Register("MinDate", typeof(DateTime), typeof(Projects), new PropertyMetadata(0));
+
+
+    }
+}
